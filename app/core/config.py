f5b5051e78a7c329822f59a1f68e5dc2e00d89c7
geeksky777 <nenabe9620@efpaper.com>
2025-09-13 10:52:47 +0300

    add docker(postgres), makefile, config, create  session for db(get_db)

---
+++ app/core/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5432
+    SECRET_KEY: str
+
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
---
+++ app/db/session.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
+from typing import AsyncGenerator
+from app.core.config import settings
+
+engine = create_async_engine(
+    url=settings.DATABASE_URL,
+    echo=True,
+)
+
+async_session_fabric = async_sessionmaker(
+    bind=engine,
+    autoflush=False,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_fabric() as session:
+        yield session
